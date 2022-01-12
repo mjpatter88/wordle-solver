9f8a97b8fa5a44b87ebfe62aa467be1ad9a63c33
Michael Patterson <mjpatter88@gmail.com>
2022-01-12 09:28:43 -0600

    Close instructions modal and input letters.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,27 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--window-size=1200,1200']
-  });
-  const page = await browser.newPage();
-  page.setViewport({width: 1200, height:1000});
-  await page.goto('https://www.powerlanguage.co.uk/wordle/', { waitUntil: 'domcontentloaded' });
-  // Wait a second after page load so all elements are ready for interaction.
-  await page.waitForTimeout(1000);
-  console.log('Page finished loading.')
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--window-size=1200,1200']
+    });
+    const page = await browser.newPage();
+    page.setViewport({ width: 1200, height: 1000 });
+    await page.goto('https://www.powerlanguage.co.uk/wordle/', { waitUntil: 'domcontentloaded' });
+    // Wait a second after page load so all elements are ready for interaction.
+    await page.waitForTimeout(1000);
+    console.log('Page finished loading.')
 
-  await browser.close();
+    // The 'pierce' handler is necessary to select elements in the shadow dom.
+    let closeHandle = await page.$('pierce/.close-icon');
+    if(closeHandle) {
+        console.log("Clicking close button to dismiss instructions...");
+        await closeHandle.click();
+    }
+    await page.keyboard.type("abc");
+
+    let tileHandles = await page.$$('pierce/.tile');
+    console.log(tileHandles.length)
 })();
 
 
